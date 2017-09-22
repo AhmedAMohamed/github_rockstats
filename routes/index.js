d643e0a5d12f09d219b9b9fe7de0cd28cb83c4d5
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-09-22 16:58:06 +0300

    project skilaton
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.json("home page here");
+    res.json('home page here');
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+
+var APIs = require('../Utils/apis');
+var reserved_words = require('../Strings/reserved_words');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    var user_name = req.headers.search_name;
+    var api = new APIs(reserved_words.search_users, [user_name, "repos"]);
+    var query_string = api.create_query();
+
+    res.send('respond with a resource');
 });
 
 module.exports = router;
