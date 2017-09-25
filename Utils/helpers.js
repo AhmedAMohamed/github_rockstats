719a2b3ae4effb23a913fafb1724f8466ae80a22
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-09-25 17:39:47 +0300

    testing
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- Utils/helpers.js
+++ Utils/helpers.js
@@ -9,7 +9,10 @@ var helpers = {
         return user;
     },
     getUserInfos : function(username) {
-        var gh = Octokit.new({token:"ef3b033a9af550851ca5c151c7bca73facd777f3"});
+        var gh = Octokit.new({
+            client_id: "b47401ec6f6283b359cc",
+            client_secret: secret_tokens.github_api_token
+        });
         var user = gh.getUser(username);
         return user.getInfo();
     }
