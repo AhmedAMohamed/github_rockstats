bc307f045d9f2b16bcd63b047a6e455c421ea3e5
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-09-25 17:11:18 +0300

    testing for deployment
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -12,50 +12,52 @@ var reserved_words = require('../Strings/reserved_words');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    var user_name = req.headers.search_name;
-    var api = new APIs(reserved_words.search_users, [user_name, "repos"]);
-    var query = api.create_query();
-    request_promise(query)
-    .then(function(results) {
-        var users_data = slice(results.items, 0, 5);
-        var users_usernames = users_data.map(function(user) {
-            return filter_obj(user, ['login']);
-        });
-        var user_promises = users_usernames.map(function(username) {
-            return Helpers.getUserInfos(username.login);
-        });
-        Promise.all(user_promises)
-        .then(function(users_data) {
-            users_data = users_data.map(function(user) {
-                var filtered_user = filter_obj(user,
-                    ['avatar_url', 'public_repos', 'public_gists', 'followers', 'following', 'bio', 'login', 'name']);
-                return Helpers.calculateScore(filtered_user);
-            });
-            users_data.map(function(user) {
-                console.log(user.bio);
-            });
-            users_data.sort(function(a,b) {
-                if (a.popularity_score > b.popularity_score) {
-                    return -1;
-                }
-                else if (a.popularity_score < b.popularity_score) {
-                    return 1;
-                }
-            });
-            var response = {
-                "valid" : true,
-                "result": users_data
-            };
-            res.json(response)
-        })
-        .catch(function(err) {
-            res.json("error 3");
-        })
-    })
-    .catch(function(err) {
-        console.log("error 2");
-        res.json(err);
-    })
+//    var user_name = req.headers.search_name;
+//    var api = new APIs(reserved_words.search_users, [user_name, "repos"]);
+//    var query = api.create_query();
+//    request_promise(query)
+//    .then(function(results) {
+//        var users_data = slice(results.items, 0, 5);
+//        var users_usernames = users_data.map(function(user) {
+//            return filter_obj(user, ['login']);
+//        });
+//        var user_promises = users_usernames.map(function(username) {
+//            return Helpers.getUserInfos(username.login);
+//        });
+//        Promise.all(user_promises)
+//        .then(function(users_data) {
+//            users_data = users_data.map(function(user) {
+//                var filtered_user = filter_obj(user,
+//                    ['avatar_url', 'public_repos', 'public_gists', 'followers', 'following', 'bio', 'login', 'name']);
+//                return Helpers.calculateScore(filtered_user);
+//            });
+//            users_data.map(function(user) {
+//                console.log(user.bio);
+//            });
+//            users_data.sort(function(a,b) {
+//                if (a.popularity_score > b.popularity_score) {
+//                    return -1;
+//                }
+//                else if (a.popularity_score < b.popularity_score) {
+//                    return 1;
+//                }
+//            });
+//            var response = {
+//                "valid" : true,
+//                "result": users_data
+//            };
+//            res.json(response)
+//        })
+//        .catch(function(err) {
+//            res.json("error 3");
+//        })
+//    })
+//    .catch(function(err) {
+//        console.log("error 2");
+//        res.json(err);
+//    })
+
+res.json("hello");
 });
 
 module.exports = router;
