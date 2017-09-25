fd4eae3b74707fc37e2729ca52e37d52acc53e2b
Ahmed Alaa <ahmedalaa3307023@gmail.com>
2017-09-25 17:28:03 +0300

    testing for deployment
    Signed-off-by: Ahmed Alaa <ahmedalaa3307023@gmail.com>

--- Strings/secrets.js
+++ Strings/secrets.js
@@ -1,6 +1,6 @@
 
 var secret_tokens = {
-    github_api_token: "ef3b033a9af550851ca5c151c7bca73facd777f3"
+    github_api_token:"6662905d7b3e643d8af3e43cfa26d416a3178272"// "ef3b033a9af550851ca5c151c7bca73facd777f3"
 }
 
 module.exports = secret_tokens;
